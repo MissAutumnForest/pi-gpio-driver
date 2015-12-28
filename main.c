c65b6130085964224772f3ea1b989ff87103f8a9
Timothy Peacock <staticinteger@gmail.com>
2015-12-28 04:31:43 +0000

    Fixed driver for new raspberry pi 2. Also made the code a lot simpler

--- main.c
+++ main.c
@@ -7,120 +7,58 @@
   
 #include <unistd.h>
 
-#define BCM2708_PERI_BASE	0x20000000
-#define GPIO_BASE		(BCM2708_PERI_BASE + 0x200000) // GPIO Controller memeory space
+#define PERI_BASE		0x3F000000 // Peripheral memory space
+#define GPIO_BASE		(PERI_BASE + 0x200000) // GPIO pins offset inside of the peripheral memory space
 
-#define BLOCK_SIZE		(4*1024)
-#define GPIO_COUNT		10
+#define INP_GPIO(g)		*(gpio + ((g)/10)) &= ~(7<<(((g)%10)*3)) // Set GPIO pin "g" to input mode
+#define OUT_GPIO(g)		*(gpio + ((g)/10)) |=  (1<<(((g)%10)*3)) // Set GPIO pin "g" to output mode
+#define SET_GPIO_ALT(g,a)	*(gpio + (((g)/10))) |= (((a)<=3?(a) + 4:(a)==4?3:2)<<(((g)%10)*3)) // Set GPIO pin "g" to alternate mode "a"
+#define GPIO_SET		*(gpio + 7) // Set the value of pin (sets if 1 ignores if 0) Format: 1 << "Pin #) EX: GPIO_SET = 1 << 23
+#define GPIO_CLR		*(gpio + 10) // Clears the value of pin (Sets if 1 ignores if 0) Format: 1 << "Pin #) EX: GPIO_CLR = 1 << 23
+#define GPIO_READ(g)		*(gpio + 13) &= (1<<(g)) // Reads the value of pin "g"
 
-#define MODE_IN			7
-#define MODE_OUT		1
 
-// #define INP_GPIO(g)		*(gpio.addr + ((g)/10)) &= ~(7<<(((g)%10)*3))
-// #define OUT_GPIO(g)		*(gpio.addr + ((g)/10)) |=  (1<<(((g)%10)*3))
-// #define SET_GPIO_ALT(g,a)	*(gpio.addr + (((g)/10))) |= (((a)<=3?(a) + 4:(a)==4?3:2)<<(((g)%10)*3))
+#define BLOCK_SIZE	(4*1024)
 
-#define GPIO_SET  *(gpio.addr + 7)  // sets   bits which are 1 ignores bits which are 0
-#define GPIO_CLR  *(gpio.addr + 10) // clears bits which are 1 ignores bits which are 0
- 
-#define GPIO_READ(g)  *(gpio.addr + 13) &= (1<<(g))
+int mem_fd;
+char *gpio_mem, *gpio_map;
 
-// IO Access to the pins
-struct bcm2835_peripheral
+volatile unsigned *gpio;
+
+int main(int argc, char *argv[])
 {
-	unsigned long addr_p;
-	int mem_fd;
-	void *map;
-	volatile unsigned int *addr;
-};
-
-struct bcm2835_peripheral gpio = {GPIO_BASE};
-
-// Our interactions with the hardware.
-int SET_GPIO_MODE(int PIN, char MODE)
-{
-	volatile unsigned int	GPIN_GROUP = *(gpio.addr + ((PIN)/10));
-	unsigned int		GPIN_LOCATION = (((PIN)%10)*3);
-	unsigned int		GPIN_VALUE;
-
-	if(MODE == 'I' || MODE == 'i')
-	{
-		GPIN_VALUE = ~(MODE_IN<<GPIN_LOCATION);
-		GPIN_GROUP &= GPIN_VALUE;
-	}
-	else if (MODE == 'O' || MODE == 'o')
-	{
-		// We need to set it to be in input mode before output mode.
-		GPIN_VALUE = ~(MODE_IN<<GPIN_LOCATION);
-		GPIN_GROUP &= GPIN_VALUE;
-
-		// Actually set it output mode.
-		GPIN_VALUE = (MODE_OUT<<GPIN_LOCATION);
-		GPIN_GROUP |= GPIN_VALUE;
-	}
-	else
-	{
-		printf("Wrong MODE Type. Expected I|O\n");
-		return -1;
-	}
-
-	return 0;
-}
-
-// Map the absolute addresses to the user space.
-int map_peripheral(struct bcm2835_peripheral *p)
-{
-
-	if((p->mem_fd = open("/dev/mem", O_RDWR|O_SYNC)) < 0)
+	if((mem_fd = open("/dev/mem", O_RDWR|O_SYNC)) < 0)
 	{
 		printf("Failed to open /dev/mem, try checking permissions.\n");
 		return -1;
 	}
 	
-	p->map = mmap(
+	gpio_map = mmap(
 		NULL,
 		BLOCK_SIZE,
 		PROT_READ | PROT_WRITE,
 		MAP_SHARED,
-		p->mem_fd,
-		p->addr_p
+		mem_fd,
+		GPIO_BASE
 	);
 
-	if (p->map == MAP_FAILED)
+	if (gpio_map == MAP_FAILED)
 	{
 		perror("Can't map memory!");
 		return -1;
 	}
 
-	p->addr = (volatile unsigned int *)p->map;
+	gpio = (volatile unsigned *)gpio_map;
 
-	return 0;
-}
+	OUT_GPIO(23);
 
-void unmap_peripheral(struct bcm2835_peripheral *p)
-{
-	munmap(p->map, BLOCK_SIZE);
-	close(p->mem_fd);
-}
-
-int main(int argc, char *argv[])
-{
-	if(map_peripheral(&gpio) == -1)
+	while(1 == 1)
 	{
-		printf("Failed to map the physical GPIO registers into the virtual memory space.\n");
-		return -1;
-	}
-
-	SET_GPIO_MODE(4, 'O');
-
-	while(1)
-	{
-		GPIO_SET = 1 << 4;
+		GPIO_SET = 1 << 23;
 		sleep(1);
-
-		GPIO_CLR = 1 << 4;
+		
+		GPIO_CLR = 1 << 23;
 		sleep(1);
-
 	}
 
 	return 0;
